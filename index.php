4249176b25ef04877db59e70c45a2147ab82137c
FishefM <yucefhernandez@hotmail.com>
2024-12-03 20:18:41 -0600

    Modificacion de datos en paginas

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
     $ctrl = new CtrlPaginaPrincipal();
     require_once "_controller/condomino/CtrlInvitados.php";
     $ctrl = new CtrlInvitados();
-    require_once "_controller/condomino/eventos/CtrlReservarEventos.php";
-    $ctrl = new CtrlReservarEventos();
-    require_once "_controller/admin/reporte_visitas/CtrlReporteVisitas.php";
-    $ctrl = new CtrlReporteVisitas();
 
     include "_view/master.html";
 ?>
